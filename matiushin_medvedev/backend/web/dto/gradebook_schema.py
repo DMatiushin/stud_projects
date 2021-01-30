fe25dc15a209b2c6ef6ad0e3995741a61bae4528
Dmitrii Matiushin <dmitrii.matiushin@leroymerlin.ru>
2021-01-30 11:20:27 +0300

    Remove validation

--- matiushin_medvedev/backend/web/dto/gradebook_schema.py
+++ matiushin_medvedev/backend/web/dto/gradebook_schema.py
@@ -6,4 +6,4 @@ class GradebookSchema(Schema):
     student_id = fields.Integer(min=1, required=True)
     educational_plan_id = fields.Integer(min=1, required=True)
     year = fields.Integer(validate=Range(min=2000), required=True)
-    mark = fields.Integer(validate=Range(min=0, max=5), required=True)
+    mark = fields.Integer()
